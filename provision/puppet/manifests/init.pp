6885d8cd6f2aad9f91a2384f07b22683e0868ca9
Petroianu Daniel <petroianudaniel@gmail.com>
2013-09-04 21:35:42 +0300

    Added index.html and cleared init.pp

--- provision/puppet/manifests/init.pp
+++ provision/puppet/manifests/init.pp
@@ -1,11 +1,1 @@
-Exec["apt-get-update"] -> Package <| |>
-
-Exec {
-    path => "/usr/bin:/usr/sbin:/bin"
-}
-
-exec { "apt-get-update" :
-    command => "/usr/bin/apt-get update"
-}
-
 group { "puppet": ensure => "present" }
